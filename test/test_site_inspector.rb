4693dfa00109974a1b6a5df5a7035b10da72bfd2
Ben Balter <ben.balter@github.com>
2014-04-10 11:27:15 -0700

    add tests

--- test/test_site_inspector.rb
+++ test/test_site_inspector.rb
@@ -1,7 +1,29 @@
 require File.join(File.dirname(__FILE__), 'helper')
 
-class TestGman < Test::Unit::TestCase
-  should "" do
+class TestSiteInspector < Test::Unit::TestCase
+  should "parse a domain" do
+    site = SiteInspector.new "foo.gov"
+    assert_equal "foo.gov", site.domain.to_s
+  end
 
+  should "parse a domain with a scheme" do
+    site = SiteInspector.new "http://foo.gov"
+    assert_equal "foo.gov", site.domain.to_s
+  end
+
+  should "parse a domain with a path" do
+    site = SiteInspector.new "foo.gov/bar"
+    assert_equal "foo.gov", site.domain.to_s
+  end
+
+  should "parse a domain with a path and scheme" do
+    site = SiteInspector.new "http://foo.gov/bar"
+    assert_equal "foo.gov", site.domain.to_s
+  end
+
+  should "generate a URI with a scheme" do
+    site = SiteInspector.new "foo.gov"
+    assert_equal "http://foo.gov", site.uri.to_s
+    assert_equal "https://foo.gov", site.uri("https").to_s
   end
 end
