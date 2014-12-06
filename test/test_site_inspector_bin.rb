4331f7f5f703bb41a38f770da461acd9faa55f29
Ben Balter <ben.balter@github.com>
2014-12-06 13:13:43 -0500

    add tests

---
+++ test/test_site_inspector_bin.rb
@@ -0,0 +1,11 @@
+require File.join(File.dirname(__FILE__), 'helper')
+
+class TestSiteInspectorBin < Minitest::Test
+  should "show usage information" do
+    assert `bundle exec site-inspector` =~ /usage/i
+  end
+
+  should "output the hash" do
+    assert `bundle exec site-inspector whitehouse.gov` =~ /government: true/i
+  end
+end
