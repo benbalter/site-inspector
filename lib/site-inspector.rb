3b07bc70b44352e602639585856510897fd0f74c
Eric Mill <eric@konklone.com>
2015-04-19 21:58:07 -0400

    proper HSTS parser with a test suite

--- lib/site-inspector.rb
+++ lib/site-inspector.rb
@@ -25,6 +25,56 @@ class SiteInspector
     YAML.load_file File.expand_path "./data/#{name}.yml", File.dirname(__FILE__)
   end
 
+  # Utility parser for HSTS headers.
+  # RFC: http://tools.ietf.org/html/rfc6797
+  def self.hsts_parse(header)
+    directives = header.split(/\s*;\s*/)
+
+    pairs = []
+    directives.each do |directive|
+      name, value = directive.downcase.split("=")
+      pairs.push([name, value])
+    end
+
+    # reject invalid directives
+
+    fatal = pairs.any? do |name, value|
+      # TODO: more comprehensive rejection of characters
+      invalid_chars = /[\s]/
+      (name =~ invalid_chars) or (value =~ invalid_chars)
+    end
+
+    if fatal
+      # good DAY, sir
+      return {
+        max_age: nil,
+        include_subdomains: false,
+        preload: false,
+        enabled: false,
+        preload_ready: false
+      }
+    end
+
+    max_age_directive = pairs.find {|n, v| n == "max-age"}
+    max_age = max_age_directive ? max_age_directive[1].to_i : nil
+    include_subdomains = !!pairs.find {|n, v| n == "includesubdomains"}
+    preload = !!pairs.find {|n, v| n == "preload"}
+
+    enabled = !!(max_age and (max_age > 0))
+
+    # Google's minimum max-age for automatic preloading
+    eighteen_weeks = !!(max_age and (max_age >= 10886400))
+    preload_ready = !!(eighteen_weeks and include_subdomains and preload)
+
+    {
+      max_age: max_age,
+      include_subdomains: include_subdomains,
+      preload: preload,
+      enabled: enabled,
+      preload_ready: preload_ready
+    }
+  end
+
   # makes no network requests
   def initialize(domain, options = {})
     domain = domain.downcase
