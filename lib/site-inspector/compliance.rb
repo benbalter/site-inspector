1bb2e9d3061851813333cb9bfea0b5a8f18e2505
Ben Balter <ben.balter@github.com>
2015-04-22 19:43:01 -0400

    fix compliance

--- lib/site-inspector/compliance.rb
+++ lib/site-inspector/compliance.rb
@@ -1,19 +1,22 @@
 class SiteInspector
+  class Domain
+    def slash_data?
+      @slash_data ||= path_exists?("/data")
+    end
+
+    def slash_developer?
+      @slash_developer ||= (path_exists?("/developer") || path_exists?("/developers"))
+    end
+
+    def data_dot_json?
+      @data_dot_json ||= path_exists?("/data.json")
+    end
+  end
+
+  private
 
   def path_exists?(path)
-    url = URI.join uri, path
-    Typhoeus::Request.get(url, followlocation: true, timeout: 10).success?
-  end
-
-  def slash_data?
-    @slash_data ||= path_exists?("/data")
-  end
-
-  def slash_developer?
-    @slash_developer ||= (path_exists?("/developer") || path_exists?("/developers"))
-  end
-
-  def data_dot_json?
-    @data_dot_json ||= path_exists?("/data.json")
+    url = URI.join canonical_endpoint.uri, path
+    canonical_endpoint.request.get(url, followlocation: true).success?
   end
 end
