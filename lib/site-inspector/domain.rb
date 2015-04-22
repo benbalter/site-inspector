ed63085823ff9de68b0b807549e8f35a50464180
Ben Balter <ben.balter@github.com>
2015-04-22 19:09:02 -0400

    convert http and http_endpoint

---
+++ lib/site-inspector/domain.rb
@@ -0,0 +1,175 @@
+class SiteInspector
+  class Domain
+
+    attr_reader :host
+
+    def initialize(host)
+      host = host.downcase
+      host = host.sub /^https?\:/, ""
+      host = host.sub /^\/+/, ""
+      host = host.sub /^www\./, ""
+      uri = Addressable::URI.parse "//#{host}"
+      @host = uri.host
+    end
+
+    def endpoints
+      @endpoints ||= [
+        Endpoint.new("https://#{host}"),
+        Endpoint.new("https://www.#{host}"),
+        Endpoint.new("http://#{host}"),
+        Endpoint.new("http://www.#{host}")
+      ]
+    end
+
+    def connonical_endpoint
+      @connonical_endpoint ||= endpoints.find do |e|
+        e.https? == connonically_https? && e.www? == connonically_www?
+      end
+    end
+
+    def government?
+      require 'gman'
+      Gman.valid? host
+    end
+
+    # Does *any* endpoint return a 200 response code?
+    def up?
+      endpoints.any? { |e| e.up? }
+    end
+
+    # Does any www endpoint return a 200 response code?
+    def www?
+      endpoints.any? { |e| e.www? && e.up? }
+    end
+
+    # Can you connect without www?
+    def root?
+      endpoints.any? { |e| e.root? && e.up? }
+    end
+
+    # HTTPS is "supported" (different than "canonical" or "enforced") if:
+    #
+    # * Either of the HTTPS endpoints is listening, and doesn't have
+    #   an invalid hostname.
+    def https?
+      endpoints.any? { |e| e.https? && e.up && e.https_valid? }
+    end
+
+    # HTTPS is enforced if one of the HTTPS endpoints is "live",
+    # and if both *HTTP* endpoints are either:
+    #
+    #  * down, or
+    #  * redirect immediately to HTTPS.
+    #
+    # This is different than whether a domain is "canonically" HTTPS.
+    #
+    # * an HTTP redirect can go to HTTPS on another domain, as long
+    #   as it's immediate.
+    # * a domain with an invalid cert can still be enforcing HTTPS.
+    def enforces_https?
+      return false unless https?
+      endpoints.select? { |e| e.http? }.all? { |e| e.down? || (e.redirect && e.redirect.https?) }
+    end
+
+    # we can say that a canonical HTTPS site "defaults" to HTTPS,
+    # even if it doesn't *strictly* enforce it (e.g. having a www
+    # subdomain first to go HTTP root before HTTPS root).
+    def defaults_https?
+      raise "Not implemented. Halp?"
+    end
+
+    # HTTPS is "downgraded" if both:
+    #
+    # * HTTPS is supported, and
+    # * The 'canonical' endpoint gets an immediate internal redirect to HTTP.
+    def downgrades_https?
+      return false unless https?
+      connonical_endpoint.redirect && connonical_endpoint.redirect.http?
+    end
+
+    # A domain is "canonically" at www if:
+    #  * at least one of its www endpoints responds
+    #  * both root endpoints are either down ~~or redirect *somewhere*~~, or
+    #  * at least one root endpoint redirect should immediately go to
+    #    an *internal* www endpoint
+    # This is meant to affirm situations like:
+    #   http:// -> https:// -> https://www
+    #   https:// -> http:// -> https://www
+    # and meant to avoid affirming situations like:
+    #   http:// -> http://non-www,
+    #   http://www -> http://non-www
+    # or like:
+    #   https:// -> 200, http:// -> http://www
+    def canonically_www?
+      # Does any endpoint respond?
+      return false unless up?
+
+      # Does at least one www endpoint respond?
+      return false unless www?
+
+      # Are both root endpoints down?
+      return true if endpoints.select { |e| e.root? }.all? { |e| e.down? }
+
+      # Does either root endpoint redirect to a www endpoint?
+      endpoints.select { |e| e.root? }.any? { |e| e.redirect && e.redirect.www? }
+    end
+
+    # A domain is "canonically" at https if:
+    #  * at least one of its https endpoints is live and
+    #    doesn't have an invalid hostname
+    #  * both http endpoints are either down or redirect *somewhere*
+    #  * at least one http endpoint redirects immediately to
+    #    an *internal* https endpoint
+    # This is meant to affirm situations like:
+    #   http:// -> http://www -> https://
+    #   https:// -> http:// -> https://www
+    # and meant to avoid affirming situations like:
+    #   http:// -> http://non-www
+    #   http://www -> http://non-www
+    # or:
+    #   http:// -> 200, http://www -> https://www
+    #
+    # It allows a site to be canonically HTTPS if the cert has
+    # a valid hostname but invalid chain issues.
+    def canonically_https?
+      # Does any endpoint respond?
+      return false unless up?
+
+      # At least one of its https endpoints is live and doesn't have an invalid hostname
+      return false unless https?
+
+      # Both http endpoints are down
+      return true if endpoints.select { |e| e.http? }.all? { |e| e.down? }
+
+      # at least one http endpoint redirects immediately to https
+      endpoints.select { |e| e.http? }.any? { |e| e.redirect && e.redirect.https? }
+    end
+
+    # A domain redirects if
+    # 1. At least one endpoint is an external redirect, and
+    # 2. All endpoints are either down or an external redirect
+    def redirect?
+      return false unless redirect
+      endpoints.all? { |e| e.down || e.external_redirect? }
+    end
+
+    # The first endpoint to respond with a redirect
+    def redirect
+      endpoints.find { |e| e.external_redirect? }
+    end
+
+    # HSTS on the canonical domain?
+    def hsts?
+      connonical_endpoint.hsts && connonical_endpoint.hsts.enabled?
+    end
+
+    def hsts_subdomains?
+      endpoints.find { |e| e.root? && e.https? }.hsts.include_subdomains?
+    end
+
+    def hsts_preload_ready?
+      return false unless hsts_subdomains?
+      endpoints.find { |e| e.root? && e.https? }.hsts.preload_ready?
+    end
+  end
+end
---
+++ lib/site-inspector/endpoint.rb
@@ -0,0 +1,151 @@
+class SiteInspector
+
+  # Every domain has four possible "endpoints" to evaluate
+  #
+  # For example, if you had `example.com` you'd have:
+  #   1. `http://example.com`
+  #   2. `http://www.example.com`
+  #   3. `https://example.com`
+  #   4. `https://www.example.com`
+  #
+  # Because each of the four endpoints could potentially respond differently
+  # We must evaluate all four to make certain determination
+  class Endpoint
+    attr_accessor :host, :uri
+
+    # Initatiate a new Endpoint object
+    #
+    # endpoint - (string) the endpoint to query (e.g., `https://example.com`)
+    def initialize(host)
+      @uri = Addressable::URI.parse(host.downcase)
+      @host = uri.host.sub(/^www\./, "")
+    end
+
+    def www?
+      @host =~ /^www\./
+    end
+
+    def root?
+      !www?
+    end
+
+    def https?
+      @uri.scheme == "https"
+    end
+
+    def http?
+      !https?
+    end
+
+    def scheme
+      @uri.scheme
+    end
+
+    def uri
+      uri = Addressable::URI.new
+      uri.host = www? ? "www.#{host}" : host
+      uri.scheme = https? ? "https" : "http"
+      uri
+    end
+
+    def request(options = {})
+      options = {
+        :timeout        => SiteInspector.timeout,
+        :followlocation => false
+      }.merge(options)
+      Typhoeus.get(uri, options)
+    end
+
+    # Makes a GET request of the given host
+    #
+    # Retutns the Typhoeus::Response object
+    def response
+      @response ||= request
+    end
+
+    # Does the endpoint return a response code between 200 and 300?
+    def up?
+      response && response_code.start_with?("2")
+    end
+
+    def down?
+      !up?
+    end
+
+    def response_code
+      response.response_code.to_s if response
+    end
+
+    def headers
+      @headers ||= Hash[response.headers.map{ |k,v| [k.downcase,v] }
+    end
+
+    def timed_out?
+      response && response.timed_out?
+    end
+
+    def https_valid?
+      https? && response && response.return_code == :ok
+    end
+
+    def https_bad_chain?
+      https? && response && response.return_code == :ssl_cacert
+    end
+
+    def https_bad_name?
+      https? && response && response.return_code == :peer_failed_verification
+    end
+
+    def hsts
+      @hsts ||= Hsts.new(headers["strict-transport-security"]) if https_valid?
+    end
+
+    # If the domain is a redirect, what's the first endpoint we're redirected to?
+    def redirect
+      return unless response && response_code.start_with?("3")
+
+      @redirect ||= begin
+        redirect = Addressable::URI.parse(headers["location"])
+
+        # This is a relative redirect, but we still need the absolute URI
+        if redirect.relative?
+          redirect.host = host
+          redirect.scheme = scheme
+        end
+
+        # This was a redirect to a subpath or back to itself, which we don't care about
+        return if redirect.host == host && redirect.scheme == scheme
+
+        # Init a new endpoint representing the redirect
+        Endpoint.new(redirect.to_s)
+      end
+    end
+
+    # Does this endpoint return a redirect?
+    def redirect?
+      !!redirect
+    end
+
+    # What's the effective URL of a request to this domain?
+    def resolves_to
+      return unless redirect?
+      @resolves_to ||= begin
+        url = request(:followlocations => true).effective_url
+        Endpoint.new(url)
+      end
+    end
+
+    def external_redirect?
+      uri != resolves_to.uri
+    end
+
+    def doc
+      require 'nokogiri'
+      @doc ||= Nokogiri::HTML response.body if response
+    end
+
+    def body
+      doc.to_s.force_encoding("UTF-8").encode("UTF-8", :invalid => :replace, :replace => "")
+    end
+  end
+end
