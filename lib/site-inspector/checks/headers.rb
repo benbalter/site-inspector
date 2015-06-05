9f161b952b720f8d8426bf2f206ae72d156d42a1
jeremie0 <jeremie@monkey.org>
2015-06-05 12:54:37 -0500

    Update headers.rb
    Remove duplicate key for :click_jacking_protection. Existing code was throwing a warning.

--- lib/site-inspector/checks/headers.rb
+++ lib/site-inspector/checks/headers.rb
@@ -72,7 +72,6 @@ class SiteInspector
           :strict_transport_security => strict_transport_security || false,
           :content_security_policy => content_security_policy || false,
           :click_jacking_protection => click_jacking_protection || false,
-          :click_jacking_protection => click_jacking_protection || false,
           :server => server,
           :xss_protection => xss_protection || false,
           :secure_cookies => secure_cookies?
